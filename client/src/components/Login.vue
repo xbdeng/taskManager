632208c03387759997f36ba8dc0a84b291c13f94
Unnamed-1408 <1210552276@qq.com>
2021-10-10 21:06:54 +0800

    Add Profile Page

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,39 +1,40 @@
 <template>
 
   <div id="login">
-        <el-row  type="flex"  justify="center" id="login_container_pos">
-            <el-col :span="12">
-              <LoginContainer></LoginContainer>
-            </el-col>
-        </el-row>
+    <el-row type="flex" justify="center" id="login_container_pos">
+      <el-col :span="12">
+        <LoginContainer></LoginContainer>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 import LoginContainer from './LoginContainer'
+
 export default {
 
   name: "Login",
-  components:{
+  components: {
     LoginContainer,
   }
-  
+
 }
 </script>
 
 <style scoped>
 
-      #login{
-        background: url("../assets/login_background.gif");
-        width: 100%;
-        height: 100%;
-        position: fixed;
-        background-size: 100% 100%;
-    }
+#login {
+  background: url("../assets/login_background.gif");
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-size: 100% 100%;
+}
 
-      #login_container_pos{
-        position: relative;
-        top:10%;
-      }
+#login_container_pos {
+  position: relative;
+  top: 10%;
+}
 
 </style>
---
+++ client/src/components/profile.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="profile">
+    <el-row  type="flex"  justify="center" id="profile_container_pos">
+      <el-col :span="24">
+        <profileContainer></profileContainer>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import profileContainer from './profileContainer'
+
+export default {
+  name: "Login",
+  components:{
+    profileContainer,
+  }
+
+}
+</script>
+
+<style scoped>
+
+#profile{
+  /*background: url("../assets/login_background.gif");*/
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-size: 100% 100%;
+}
+
+#profile_container_pos{
+  position: relative;
+}
+
+</style>
