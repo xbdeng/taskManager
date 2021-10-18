16089c6419f55cbf540284bae143dd6ae2be8fb2
dxb <2327619908@qq.com>
2021-10-18 13:51:31 +0800

    添加 swagger 配置

---
+++ backend/src/main/java/com/hungry/taskmanager/config/SwaggerConfig.java
@@ -0,0 +1,60 @@
+package com.hungry.taskmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2//开启Swagger2
+public class SwaggerConfig {
+    //配置Swagger的Docket的bean实例
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())//配置Swagger信息
+                .select()
+                /**
+                 * apis():指定扫描的接口
+                 *  RequestHandlerSelectors:配置要扫描接口的方式
+                 *       basePackage:指定要扫描的包
+                 *       any:扫面全部
+                 *       none:不扫描
+                 *       withClassAnnotation:扫描类上的注解(参数是类上注解的class对象)
+                 *       withMethodAnnotation:扫描方法上的注解(参数是方法上的注解的class对象)
+                 */
+                .apis(RequestHandlerSelectors.basePackage("com.hungry.taskmanager.controller"))
+                /**
+                 * paths():过滤路径
+                 *  PathSelectors:配置过滤的路径
+                 *      any:过滤全部路径
+                 *      none:不过滤路径
+                 *      ant:过滤指定路径:按照按照Spring的AntPathMatcher提供的match方法进行匹配
+                 *      regex:过滤指定路径:按照String的matches方法进行匹配
+                 */
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    //配置Swagger信息
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "task manager",
+                "task manager Swagger API文档",
+                "1.0",
+                "https://localhost:8080",
+                new Contact("Baret-H", "https://bareth.blog.csdn.net/", ""),//作者信息
+                "Apache 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<VendorExtension>());
+    }
+}
