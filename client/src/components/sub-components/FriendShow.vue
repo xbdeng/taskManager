90fa3f063ebe8d497c28bde14bdc1f714db394aa
Lee <chlijiaao@163.com>
2021-11-06 22:07:35 +0800

    finish data frame of address book, add 2dlive model

---
+++ client/src/components/sub-components/FriendShow.vue
@@ -0,0 +1,30 @@
+<template>
+<!-- 这个页面是一个模板页面，用来向用户显示好友信息 -->
+  <div>
+    <template>
+      <el-container>
+        <!-- 头部栏显示好友名称 -->
+          <el-header>
+              {{ friend.name }}
+          </el-header>
+          <!-- 主页面显示好友信息-->
+          <el-main>
+              <p>{{ friend.email }}</p>
+              <p>{{ friend.telephone }}</p>
+              <p>{{ friend.personalSign }}</p>
+          </el-main>
+      </el-container>
+    </template>
+  </div>
+</template>
+ 
+<script>
+
+export default {
+
+  name: 'FriendShow',
+  // 这个组件接收的参数是一个任务对象
+  props: ['friend'],
+  
+}
+</script>
