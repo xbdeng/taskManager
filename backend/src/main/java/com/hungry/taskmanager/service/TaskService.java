6f53545965c74dd48aaf9215ab3334603775b4da
xbdeng <2327619908@qq.com>
2021-11-18 19:01:56 +0800

    TaskService

--- backend/src/main/java/com/hungry/taskmanager/service/TaskService.java
+++ backend/src/main/java/com/hungry/taskmanager/service/TaskService.java
@@ -1,5 +1,21 @@
 package com.hungry.taskmanager.service;
 
+import com.hungry.taskmanager.entity.Task;
+import com.hungry.taskmanager.entity.post_entities.CreateTaskParams;
+import com.hungry.taskmanager.entity.post_entities.EditTaskParams;
+import com.hungry.taskmanager.entity.post_entities.QueryTaskParams;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+
 public interface TaskService {
+
+    int addTask(CreateTaskParams params) throws Exception;
+
+    int deleteTask(@NonNull long taskId) throws Exception;
+
+    List<Task> queryTask(QueryTaskParams filter) throws Exception;
+
+    int editTask(EditTaskParams params) throws Exception;
 
 }
