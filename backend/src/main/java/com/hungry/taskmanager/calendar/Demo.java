5132539d53723262b472af47bcb1727af88317cb
ComgLq24 <ComgLqWork@outlook.com>
2021-11-23 05:53:18 +0800

    alter localdatetime store in database

--- backend/src/main/java/com/hungry/taskmanager/calendar/Demo.java
+++ backend/src/main/java/com/hungry/taskmanager/calendar/Demo.java
@@ -3,9 +3,9 @@ package com.hungry.taskmanager.calendar;
 import com.nylas.NylasClient;
 
 public class Demo {
-    public static void main(String[] args) {
-        NylasClient client = new NylasClient();
-
-
-    }
+//    public static void main(String[] args) {
+//        NylasClient client = new NylasClient();
+//
+//
+//    }
 }
