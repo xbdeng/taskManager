b07e2a560061245bd16a90e7322602b9cb2053b6
Unnamed-1408 <1210552276@qq.com>
2021-11-25 22:57:57 +0800

    edit analysis

--- client/src/components/sub-components/LineChart.js
+++ client/src/components/sub-components/LineChart.js
@@ -4,11 +4,19 @@ const { reactiveProp } = mixins
 export default {
     extends: Line,
     mixins: [reactiveProp],
-    props: ['options'],
+    props: ['options', 'show'],
+    watch:{
+        show(newValue, oldValue) {
+            // console.log(this.chartData)
+            this.renderChart(this.chartData, this.options)
+        }
+    },
     mounted () {
 
         // this.chartData is created in the mixin.
         // If you want to pass options please create a local options object
-        this.renderChart(this.chartData, this.options)
+        if(this.show){
+            this.renderChart(this.chartData, this.options)
+        }
     }
 }
