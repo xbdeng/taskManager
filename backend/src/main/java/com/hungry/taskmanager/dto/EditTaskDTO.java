06a74350e44e50ec352dccab1ba27ef48c40cbe9
xbdeng <2327619908@qq.com>
2021-11-26 05:05:30 +0800

    删除无用文件

--- backend/src/main/java/com/hungry/taskmanager/dto/EditTaskDTO.java
+++ backend/src/main/java/com/hungry/taskmanager/dto/EditTaskDTO.java
@@ -1,34 +1,34 @@
-package com.hungry.taskmanager.dto;
-
-import io.swagger.annotations.ApiModelProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
-
-import java.math.BigInteger;
-import java.util.List;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Accessors(chain = true)
-public class EditTaskDTO {
-    @ApiModelProperty(hidden = true)
-    private String username;
-    private BigInteger taskId;
-    private String taskName;
-    private String description;
-    private List<String> tags;
-    private Integer privilege;
-    private String createDate;
-    private String dueDate;
-    private String remindDate;
-    private String location;
-    // other parameters necessary for modifying a task
-    private BigInteger fatherTask;
-    private List<String> subTasks;
-    private Integer status;
-
-}
+//package com.hungry.taskmanager.dto;
+//
+//import io.swagger.annotations.ApiModelProperty;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import lombok.NoArgsConstructor;
+//import lombok.experimental.Accessors;
+//
+//import java.math.BigInteger;
+//import java.util.List;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Accessors(chain = true)
+//public class EditTaskDTO {
+//    @ApiModelProperty(hidden = true)
+//    private String username;
+//    private BigInteger taskId;
+//    private String taskName;
+//    private String description;
+//    private List<String> tags;
+//    private Integer privilege;
+//    private String createDate;
+//    private String dueDate;
+//    private String remindDate;
+//    private String location;
+//    // other parameters necessary for modifying a task
+//    private BigInteger fatherTask;
+//    private List<String> subTasks;
+//    private Integer status;
+//
+//}
